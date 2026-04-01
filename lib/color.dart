3945ba7a3dcd65360081470bba7dcd713f2cbf7b
Priyalee <priyalee02@gmail.com>
2026-04-01 16:03:31 +0530

    Added Passwordprovider

--- lib/color.dart
+++ lib/color.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const LinearGradient railGradient = LinearGradient(
-    colors: [Color.fromARGB(255, 4, 46, 80), Color.fromARGB(255, 162, 27, 185)],
+    colors: [Color.fromARGB(255, 4, 46, 80), Color.fromARGB(255, 151, 21, 134)],
     begin: Alignment.topLeft,
     end: Alignment.topRight,
   );
